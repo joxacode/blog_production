8448c68ebc0aa381dc878fcbc78d69e75d99f966
joxacode <shohjahonhamidov192@gmail.com>
2023-06-15 18:42:26 +0500

    change settings

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-e^4ub@(=9_!bkc9gk1-%$j)9g$!$b5)0^ch_#dr=@428jx6p0r
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["xamidovshohjaxon.pythonanywhere.com"]
 
 
 # Application definition
